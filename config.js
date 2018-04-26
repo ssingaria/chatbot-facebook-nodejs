cebdf3fc5fef1df472aec7bd109e510a6162a81f
ssingaria <38689985+ssingaria@users.noreply.github.com>
2018-04-26 17:00:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     FB_PAGE_TOKEN: 'EAAGRfDZAL6DUBACW4tVGxCgOOfhbwDVjNNLyrIAedB83UB5D6oZB3FMow82sN4os9iW18gdYPBhggZCpefKAZAg5naqwM4TvOl5fnGVBxhcZC5H8CJvgv6GO4VI7nJAQOvfEws8JLuglAsfPVrW4abl0jnDaMtRt4u7krNOzIHAZDZD',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_VERIFY_TOKEN: 'iwanttotestusingheroku',
+    API_AI_CLIENT_ACCESS_TOKEN: 'c5483e4a0a4c44f594703ad3e17469bb',
+    FB_APP_SECRET: '5e17a436bfe289837d51819a6c14e1cb',
+    SERVER_URL: "https://pacific-lake-43294.herokuapp.com/",
 };
