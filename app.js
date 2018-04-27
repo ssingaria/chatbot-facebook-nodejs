97be4698387b875ad642566c1b96d711c8621d99
ssingaria <38689985+ssingaria@users.noreply.github.com>
2018-04-27 21:44:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -918,7 +918,7 @@ var subject = subject;
 var content = new helper.Content('text/html', content);
 var mail = new helper.Mail(from_email, subject, to_email, content);
 
-var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+var sg = require('sendgrid')(config.SENDGRID_API_KEY);
 var request = sg.emptyRequest({
   method: 'POST',
   path: '/v3/mail/send',
