712ddc3ca5e45aa2191324e03a804d26c3b2f340
ssingaria <38689985+ssingaria@users.noreply.github.com>
2018-05-01 13:30:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -218,7 +218,7 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 				if(!error && response.statusCode == 200) {
 					let weather = JSON.parse(body);
 					if (weather.hasOwnProperty("weather")) {
-						let reply = '${responseText} ${weatherconditions["weatherDesc"][0]["value"]}';
+						let reply = '${responseText} ${weather["weather"][0]["description"]}';
 						console.log("weather description: ", reply);
 						sendTextMessage(sender, reply);
 					} else {
