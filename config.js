d569504b4333728c4b0075d629f096379ac35a22
ssingaria <38689985+ssingaria@users.noreply.github.com>
2018-05-08 15:23:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ module.exports = {
     FB_PAGE_TOKEN: 'EAAGRfDZAL6DUBACW4tVGxCgOOfhbwDVjNNLyrIAedB83UB5D6oZB3FMow82sN4os9iW18gdYPBhggZCpefKAZAg5naqwM4TvOl5fnGVBxhcZC5H8CJvgv6GO4VI7nJAQOvfEws8JLuglAsfPVrW4abl0jnDaMtRt4u7krNOzIHAZDZD',
     FB_VERIFY_TOKEN: 'iwanttotestusingheroku',
     API_AI_CLIENT_ACCESS_TOKEN: 'c5483e4a0a4c44f594703ad3e17469bb',
-    FB_APP_SECRET: 'f171b9e02539b1decb4a920f8216984a',
+    FB_APP_SECRET: 'd5d5f1cd703296145a313e7ef3bf6118',
     SERVER_URL: "https://pacific-lake-43294.herokuapp.com/",
     SENGRID_API_KEY: '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',
     EMAIL_FROM: 'ssingaria@gmail.com',
     EMAIL_TO: 'ssingaria@gmail.com',
-    WEATHER_HOST_URL: "api.worldweatheronline.com",
+    WEATHER_HOST_URL: "api.openweathermap.org",
     WEATHER_API_KEY: 'cd5be522318e6a83b473640825ef7b84'
 };
